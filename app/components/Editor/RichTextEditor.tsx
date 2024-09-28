9de400a50cd3ef62a4c8f2bf2aeddb89481b1f88
Piyush Dubey <piyush115dubey@gmail.com>
2024-09-28 22:23:12 +0530

    custom label for next button

--- app/components/Editor/RichTextEditor.tsx
+++ app/components/Editor/RichTextEditor.tsx
@@ -3,7 +3,7 @@ import { Button } from "@chakra-ui/react";
 import React, { useState } from "react";
 import Editor from "react-simple-wysiwyg";
 
-const RichTextEditor = ({ goToNext }: { goToNext: () => void }) => {
+const RichTextEditor = ({ goToNext, customNextButton }: { goToNext: () => void, customNextButton?: string }) => {
   function generateRandomParagraph(numSentences: number) {
     const sentences = [
       `NALS Apartment Homes is pleased to present our
@@ -49,13 +49,13 @@ experience managing through the 2009 recessi `,
     <div className="h-[12vh]">
       <div className="flex justify-end mt-2">
         <Button onClick={goToNext} colorScheme="green">
-          Next
+          {customNextButton ? customNextButton : "Next"}
         </Button>
       </div>
       <div className="p-2 h-[10vh]">
         <Editor
           value={value}
-          containerProps={{ style: { height: "50vh",overflow:"scroll" } }}
+          containerProps={{ style: { height: "50vh", overflow: "scroll" } }}
           onChange={(e) => setValue(e.target.value)}
         />
       </div>
