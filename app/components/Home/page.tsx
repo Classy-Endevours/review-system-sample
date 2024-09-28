6e1b958e85cb3eab6f4d21c6d75e29da325ab7d3
Piyush Dubey <piyush115dubey@gmail.com>
2024-09-28 22:04:18 +0530

    removed heading from report

--- app/components/Home/page.tsx
+++ app/components/Home/page.tsx
@@ -62,17 +62,14 @@ const ReportTile: React.FC<ReportTileProps> = ({
 const Home: React.FC = () => {
   const [activeSection, setActiveSection] = useState<string>("selectReport");
   const [selectedYear, setSelectedYear] = useState<string>("2024");
-  const [selectedReport, setSelectedReport] = useState<string>("");
   const [showRegenerate, setShowRegenerate] = useState(false)
   const [showEditor, setShowEditor] = useState(false)
 
-  const handleReportSelect = (report: string) => {
-    setSelectedReport(report);
+  const handleReportSelect = () => {
     setActiveSection("reportDetails");
   };
 
-  const handleInReviewSelect = (report: string) => {
-    setSelectedReport(report);
+  const handleInReviewSelect = () => {
     setActiveSection("reportDetails");
     setShowRegenerate(true);
   };
@@ -154,9 +151,6 @@ const Home: React.FC = () => {
           className="bg-white rounded-xl shadow-sm overflow-hidden p-4"
         >
           <div className="flex justify-between">
-            <h2 className="text-lg font-medium mb-4 text-midnight-blue">
-              {selectedReport} Details
-            </h2>
             <button onClick={closeReport} type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="default-modal">
               <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
